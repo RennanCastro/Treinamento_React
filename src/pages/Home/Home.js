a9edc0e3ba7272c4626f077fc9d06de99d9c3528
RennanCastro <83237484+RennanCastro@users.noreply.github.com>
2021-05-01 00:26:33 -0300

    Passar de um site para outro

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import  { useHistory } from "react-router-dom";
 
 function Home() {
+
+  const history = useHistory();
+
   return (
     <div>
       <h1>HOME</h1>
+      <button onClick ={()=>{history.push("login");}}> 
+      Login 
+      </button>
     </div>
   );
 }
